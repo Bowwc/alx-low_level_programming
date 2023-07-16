ab2ce1672926f185da75a53408d19c177c5aefc2
Bowwc <bowwckal10@gmail.com>
2023-07-16 09:40:35 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet
+ * Description - prints alphabet
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch, ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n')
+	return (0)
+}
