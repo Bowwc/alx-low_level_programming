836dae0e6fe960fa2a38f244171fbc88a575f191
Bowwc <bowwckal10@gmail.com>
2023-07-19 19:03:07 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - a function that prints the alphabet
+* Return: void
+*/
+int main(void)
+{
+void print_alphabet(void);
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+return (void);
+}
