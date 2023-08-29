a23c776d85d3121044df2a7c0082cff35a00c834
Bowwc <bowwckal0@gmail.com>
2023-08-29 13:27:03 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,6 +1,8 @@
+#include "lists.h"
+
 /**
- * free_listint - frees a linked list
- * @head: All listint_t list to be freed
+ * free_listint -This function frees a linked list
+ * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
